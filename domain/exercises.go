bb51974ce68ca6f093f118d14c5e24133e04030c
ervitis <vitomarti@gmail.com>
2022-08-27 13:55:48 +0900

    chore: comment

--- domain/exercises.go
+++ domain/exercises.go
@@ -167,17 +167,6 @@ func (r raw) Convert() MonthWodExercises {
 			continue
 		}
 
-		/*
-			1 2 3 4 5 6 7
-			0 1 2 3 4 5 6
-
-			8 9 10 11 12 13 14
-			0 1 2  3  4  5  6
-
-			15 16 17 18 19 20 21
-			0  1  2  3  4  5  6
-		*/
-
 		weeks[day].name = ExerciseName(name)
 	}
 	return weeks
